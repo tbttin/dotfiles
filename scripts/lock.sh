e35e4bc8c5d773932310fe502bcf61ab4bde249d
Tin Thai <thaibachthanhtin@gmail.com>
2020-04-30 09:47:35 +0700

    A cleaner lock script. Fixed xss-lock.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 
+icon="$HOME/.config/i3/lock.png"
+# Create a temp file.
+img=$(mktemp /tmp/XXXXXXXXXX.png)
+
 # Take a screenshot.
-scrot -zo /tmp/screen.png
+scrot -oz $img
+#import -window root $img
 
 # Simulate oil painting.
-convert /tmp/screen.png -paint 3 /tmp/screen.png
-# Center lock image.
-[[ -f ~/.config/i3/lock-256.png ]] && convert /tmp/screen.png ~/.config/i3/lock-256.png -gravity center -composite -matte /tmp/screen.png
+#convert $img -paint 3 $img
+# Or pixelate the screenshot.
+convert $img -scale 10% -scale 1000% $img
+
+# Add the lock icon.
+[[ -f $icon ]] && convert $img $icon -gravity center -composite -matte $img
 
 # Lock
-i3lock -nuei /tmp/screen.png
+i3lock -nuei $img
 
-# Remove
-rm /tmp/screen.png
+# Remove the tmp file.
+rm $img
