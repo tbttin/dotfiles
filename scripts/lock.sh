38a173dbe855bf190e654e5d2ea03ac548707fd0
Tin Thai <thaibachthanhtin@gmail.com>
2020-07-08 16:47:31 +0700

    Update LOCK variable.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,7 +2,7 @@
 
 # Create a temp file.
 TMPBG=$(mktemp /tmp/XXXXXXXXXX.png)
-LOCK="$XDG_CONFIG_HOME/i3/lock.png"
+LOCK_ICON="$HOME/.config/i3/lock.png"
 
 # Take a screenshot.
 scrot -oz $TMPBG
@@ -11,7 +11,7 @@ scrot -oz $TMPBG
 convert $TMPBG -paint 3 $TMPBG
 
 # Add the lock icon.
-[[ -f $LOCK ]] && convert $TMPBG $LOCK -gravity center -composite $TMPBG
+[[ -f $LOCK_ICON ]] && convert $TMPBG $LOCK_ICON -gravity center -composite $TMPBG
 
 # Lock.
 i3lock -nuei $TMPBG
