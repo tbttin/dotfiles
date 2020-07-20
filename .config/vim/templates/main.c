d9b8e1b29c990dd10145e429d9dc33d385ea937c
Tin Thai <thaibachthanhtin@gmail.com>
2020-07-20 23:17:27 +0700

    Add filename, date, author, codeforce ONLINE_JUDGE define.

--- .config/vim/templates/main.c
+++ .config/vim/templates/main.c
@@ -1,9 +1,21 @@
+/*
+ *        File: [:VIM_EVAL:]expand("<afile>:t")[:END_EVAL:]
+ *  Created on: [:VIM_EVAL:]strftime("%Y %b %d")[:END_EVAL:]
+ *      Author: [:VIM_EVAL:]expand("$USER")[:END_EVAL:]
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int
-main(void)
+main (void)
 {
+#ifndef ONLINE_JUDGE
+#define C_LOG(...) printf(__VA_ARGS__)
+  freopen ("input.txt", "r", stdin);
+#else
+#define C_LOG(...)
+#endif
 
   return EXIT_SUCCESS;
 }
