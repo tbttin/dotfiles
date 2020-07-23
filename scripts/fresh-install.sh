3a70198feef448b9471827cf9a57c9de1e223125
Tin Thai <thaibachthanhtin@gmail.com>
2020-07-24 03:34:31 +0700

    Fix syntax errors.

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -1,27 +1,24 @@
-#curl -Lks https://git.io/fresh-install | /bin/bash
+# curl -Lks https://git.io/fresh-install | /bin/bash
 
 git clone --bare https://github.com/tbttin/dotfiles.git $HOME/.config/dotfiles
 function config {
     /usr/bin/git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME $@
 }
-$CONF_BACKUP=~/.config-backup
+CONF_BACKUP=~/.config-backup
 mkdir -p ${CONF_BACKUP}
 config checkout
 if [ $? = 0 ]; then
-    echo "Checked out config.";
+    echo "Checked out config."
 else
-    echo "Backing up pre-existing dot files.";
+    echo "Backing up pre-existing dotfiles."
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $CONF_BACKUP/{}
-fi;
+fi
 config checkout
 config config status.showUntrackedFiles no
-
 echo "Creating regular directories."
-mkdir ~/{documents,downloads,pictures,videos}
-
+mkdir -p ~/{documents,downloads,pictures,videos}
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
     echo "Updating xdg-user-dirs."
     xdg-user-dirs-update
-fi;
-
+fi
 echo "Done."
