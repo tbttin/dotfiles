c92385c7fd6218c7400a7968a879930b45e63ec4
Tin Thai <tbtt@arch.localdomain>
2020-10-06 02:57:25 +0700

    Update some comments.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,7 +2,7 @@
 
 # Create a temp file.
 TMPBG=$(mktemp /tmp/XXXXXXXXXX.png)
-LOCK_ICON="$HOME/.config/i3/lock.png"
+LOCK_ICON="$XDG_CONFIG_HOME/i3/lock.png"
 
 # Take a screenshot.
 import -window root -silent $TMPBG
@@ -13,7 +13,7 @@ convert $TMPBG -paint 3 $TMPBG
 # Add the lock icon.
 [[ -f $LOCK_ICON ]] && convert $TMPBG $LOCK_ICON -gravity center -composite $TMPBG
 
-# Lock.
+# Lock, nofork, no unlock indicator, ignore empty password.
 i3lock -nuei $TMPBG
 
 # Remove the temp file.
