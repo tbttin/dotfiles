388e0c59cbc95cc736459c959ea1ee8cfe78313e
Tin Thai <tbtt@arch.localdomain>
2020-12-10 03:10:01 +0700

    Rearrange some commands, add some somments.

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# curl -Lks https://git.io/fresh-install | /bin/bash
+# Download this script from github repo and execute it with bash.
+# curl -Ls https://git.io/fresh-install | /bin/bash
 
-git clone --bare https://github.com/tbttin/dotfiles.git $HOME/.config/dotfiles
 function config {
-    /usr/bin/git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME $@
+    /usr/bin/git --git-dir=${HOME}/.config/dotfiles --work-tree=${HOME} $@
 }
+
+# Clone my bare repo.
+git clone --bare https://github.com/tbttin/dotfiles.git ${HOME}/.config/dotfiles
+# Backup stock config files if they exist and install my dotfiles.
 CONFIG_BACKUP_DIR=~/.config~
 mkdir -p ${CONFIG_BACKUP_DIR}
 config checkout
@@ -16,13 +20,14 @@ else
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $CONFIG_BACKUP_DIR
 fi
 config checkout
-config config status.showUntrackedFiles no
-config config diff.tool vimdiff
-config config difftool.prompt false
 echo "Create and update regular directories."
 mkdir -p ~/{documents,downloads,pictures{,/screenshots},videos}
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
     rmdir --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
     xdg-user-dirs-update
 fi
+# Add some git configs.
+config config status.showUntrackedFiles no
+config config diff.tool vimdiff
+config config difftool.prompt false
 echo "Done."
