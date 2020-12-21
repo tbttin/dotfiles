e9fcf0c1c4f6fb71ab695bacb3177bd53ae43cf6
Tin Thai <tbtt@arch.localdomain>
2020-12-21 23:53:20 +0700

    Fix output redirection, update output messages.

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -11,20 +11,21 @@ function config {
 git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
 CONFIG_BACKUP_DIR=~/.config~
-mkdir -pv ${CONFIG_BACKUP_DIR}
-config checkout | sed 's/^/  /'
+config checkout 2>&1 | sed 's/^/  -> /'
 # Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
 if [ ${PIPESTATUS[0]} = 0 ]; then
     echo 'Checked out config.'
 else
-    echo 'Backing up pre-existing dotfiles.'
+    echo "Backing up pre-existing dotfiles to ${CONFIG_BACKUP_DIR}."
+    mkdir -pv ${CONFIG_BACKUP_DIR}
     config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | xargs -I{} mv {} $CONFIG_BACKUP_DIR
 fi
 config checkout
 config config status.showUntrackedFiles no
-echo 'Create and update regular directories.'
+echo 'Creating regular dirs.'
 mkdir -pv ~/{documents,downloads,pictures/screenshots,videos}
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
+    echo 'Deleting XDG default dirs.'
     rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
     xdg-user-dirs-update
 fi
