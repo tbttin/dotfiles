3ad13400be17a8cefe71c15ee7e9db0f2697aae8
Tin Thai <tbtt@arch.localdomain>
2020-12-21 20:47:01 +0700

    Update quotes, output indent, verbose, $PIPESTATUS, .etc

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -4,30 +4,32 @@
 # curl -Ls https://git.io/fresh-install | /bin/bash
 
 function config {
-    /usr/bin/git --git-dir=${HOME}/.config/dotfiles --work-tree=${HOME} $@
+    /usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
 }
 
-# Clone my bare repo.
-git clone --bare https://github.com/tbttin/dotfiles.git ${HOME}/.config/dotfiles
+# Clone my dotfiles repo as a bare repo.
+git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
 CONFIG_BACKUP_DIR=~/.config~
 mkdir -p ${CONFIG_BACKUP_DIR}
-config checkout
-if [ $? = 0 ]; then
-    echo "Checked out config."
+config checkout | sed 's/^/  /'
+# Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
+if [ ${PIPESTATUS[0]} = 0 ]; then
+    echo 'Checked out config.'
 else
-    echo "Backing up pre-existing dotfiles."
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $CONFIG_BACKUP_DIR
+    echo 'Backing up pre-existing dotfiles.'
+    config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | xargs -I{} mv {} $CONFIG_BACKUP_DIR
 fi
 config checkout
-echo "Create and update regular directories."
-mkdir -p ~/{documents,downloads,pictures{,/screenshots},videos}
+config config status.showUntrackedFiles no
+echo 'Create and update regular directories.'
+mkdir -p ~/{documents,downloads,pictures/screenshots,videos}
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
-    rmdir --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+    rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
     xdg-user-dirs-update
 fi
-# Add some git configs.
-config config status.showUntrackedFiles no
+# Additional configs.
+# Vimdiff as default difftool.
 config config diff.tool vimdiff
 config config difftool.prompt false
-echo "Done."
+echo 'Done.'
