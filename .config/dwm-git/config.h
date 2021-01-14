541b674c42c6b6f3feb5a349abeaa1589f6d72c9
Tin Thai <tbtt@arch.localdomain>
2021-01-14 15:59:27 +0700

    Realign some variable declarations.

--- .config/dwm-git/config.h
+++ .config/dwm-git/config.h
@@ -3,18 +3,18 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx = 1;        /* border pixel of windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+static const int showbar           = 1;        /* 0 means no bar */
+static const int topbar            = 1;        /* 0 means bottom bar */
+static const char *fonts[]         = { "monospace:size=10" };
+static const char dmenufont[]      = "monospace:size=10";
+static const char col_gray1[]      = "#222222";
+static const char col_gray2[]      = "#444444";
+static const char col_gray3[]      = "#bbbbbb";
+static const char col_gray4[]      = "#eeeeee";
+static const char col_cyan[]       = "#005577";
+static const char *colors[][3]     = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
@@ -28,10 +28,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     switchtotag    isfloating   monitor */
-	{ "firefox",  NULL,       NULL,       1 << 8,       1,             0,           -1 },
-	{ "Zathura",  NULL,       NULL,       1 << 7,       1,             0,           -1 },
-	{ "ffplay",   NULL,       NULL,       1 << 1,       0,             0,           -1 },
+	/* class        instance    title     tags mask    switchtotag    isfloating    monitor */
+	{ "firefox",    NULL,       NULL,     1 << 8,      1,             0,            -1 },
+	{ "Zathura",    NULL,       NULL,     1 << 7,      1,             0,            -1 },
+	{ "ffplay",     NULL,       NULL,     1 << 1,      0,             0,            -1 },
 };
 
 /* layout(s) */
@@ -58,15 +58,15 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
-static const char *pdfreadercmd[]  = { "zathura", NULL };
-static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
-static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
-static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
-static const char *lockcmd[]  = { "slock", NULL };
+static char dmenumon[2]           = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]      = { "st", NULL };
+static const char *browsercmd[]   = { "firefox", NULL };
+static const char *pdfreadercmd[] = { "zathura", NULL };
+static const char *mutecmd[]      = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *volupcmd[]     = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[]   = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *lockcmd[]      = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier          key                        function         argument */
