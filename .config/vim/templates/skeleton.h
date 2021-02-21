0d67a4d7cc1679d3f060852c0a9c1fbb68996cc6
Tin Thai <tbtt@arch.localdomain>
2021-02-22 01:00:20 +0700

    Add C header file template.

---
+++ .config/vim/templates/skeleton.h
@@ -0,0 +1,3 @@
+#ifndef [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED
+#define [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED
+#endif /* [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED */
