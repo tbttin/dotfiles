d6f425c1c3e685fe7f4930725e525ca20851ad1b
Tin Thai <tbtt@arch.localdomain>
2021-06-08 13:01:26 +0700

    Add a blank line.

--- .config/vim/templates/skeleton.h
+++ .config/vim/templates/skeleton.h
@@ -1,3 +1,4 @@
 #ifndef [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED
 #define [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED
+
 #endif /* [:VIM_EVAL:]toupper (expand ("<afile>:t:r"))[:END_EVAL:]_H_INCLUDED */
