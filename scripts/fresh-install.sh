9f5105a16e27044e21b2cfc5b728ba5072fcdf12
Tin Thai <tbtt@arch.localdomain>
2021-06-20 14:59:16 +0700

    Update some comments.

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Download this script from github repo and execute it with bash.
+# Download this script from github repo and execute it shell script.
 # curl -Ls https://git.io/fresh-install | /bin/sh
 
 function indent {
@@ -11,10 +11,13 @@ function config {
     /usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
 }
 
+#
+# Backup and install.
+#
 # Clone my dotfiles repo as a bare repo.
 git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
-CONFIG_BACKUP_DIR=~/.config~
+CONFIG_BACKUP_DIR="${HOME}/.config~"
 echo 'Attempt to install dotfiles to home folder.'
 config checkout 2>&1 | indent
 # Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
@@ -25,19 +28,19 @@ else
     mkdir -pv "${CONFIG_BACKUP_DIR}" | indent
     config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | xargs -I{} mv -v {} "${CONFIG_BACKUP_DIR}" | indent
 fi
-config checkout
+#
+# Checkout and update some configs.
+#
+config checkout | indent
 config config -- status.showUntrackedFiles no
+# Use vimdiff as default difftool.
+config config -- diff.tool vimdiff
+config config -- difftool.prompt false
 echo 'Creating regular dirs.'
 mkdir -pv ~/{documents,downloads,pictures/screenshots,videos} | indent
-# TODO: What is this?
-#if command -v given-command > /dev/null 2>&1; then
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
     echo 'Deleting XDG default dirs.'
     rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
     xdg-user-dirs-update
 fi
-# Additional configs.
-# Vimdiff as default difftool.
-config config -- diff.tool vimdiff
-config config -- difftool.prompt false
 echo 'Done.'
