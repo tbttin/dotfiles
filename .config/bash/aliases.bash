8b8798ffb8c60b668a460d9aa2a5e3c09a49853d
Tin Thai <thaibachthanhtin@gmail.com>
2021-11-05 13:26:47 +0700

    Remove srb alias New aliases: - pc: quick change to ~/projects/c - pv: quick change to ~/projects/vim

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -1,17 +1,18 @@
-# Some improving for movements.
+# Faster navigation.
 alias ..='cd ..'
 alias ...='cd ../..'
 # With most commands, you can pass -- as an argument,
 # and all subsequent arguments are treated as operands and not options even if they begin with a dash.
 alias -- --='cd ${OLDPWD}'
-alias c="cd $(xdg-user-dir CDEVELOPMENT)"
 alias d="cd $(xdg-user-dir DOCUMENTS)"
 alias l="cd $(xdg-user-dir DOWNLOAD)"
 alias p="cd $(xdg-user-dir PICTURES)"
 alias s="cd $(xdg-user-dir SCRIPTS)"
 alias v="cd $(xdg-user-dir VIDEOS)"
+alias pc="cd $(xdg-user-dir C)"
+alias pv="cd $(xdf-user-dir VIM)"
 
-# Quick edit some config files.
+# Quick open some config files.
 # Bashes.
 alias vb='vim ~/.bashrc'
 alias vp='vim ~/.bash_profile'
@@ -47,9 +48,8 @@ alias um16='doas umount /mnt/16g-usb'
 # System stuffs.
 alias dapm='doas pacman'
 alias spo='systemctl poweroff -i'
-alias srb='systemctl reboot'
 
-# Dotfiles git bare aliases.
+# Bare git dotfiles aliases.
 alias config='/usr/bin/git --work-tree=${HOME} --git-dir=${HOME}/.config/dotfiles/'
 alias cfs='config status'
 alias cfd='config diff'
@@ -65,15 +65,16 @@ alias ffplay='ffplay -hide_banner -nostats -seek_interval 5 -autoexit'
 # Seeking for file infomation.
 alias ffprobe='ffprobe -hide_banner'
 
-# startx with xinitrc in $XDG_CONFIG_HOME.
+# Start X with xinitrc in $XDG_CONFIG_HOME.
 alias startx='startx ${XINITRC}'
 
 # Display all locations containing an executable named includes aliases, builtins, and functions.
 alias type='type -a'
 
+# Notes {{{1
 # Aliases are created and listed with the alias command, and removed with the unalias command.
 # Do not overuse alias, we'll lose autocompletion ability.
-# '' vs ""
-#   Single quotes are evaluated dynamically.
-#   Double quotes are evaluated at time of creation and, thereafter, never changes.
-      #alias QS="echo \$PWD"
+# Single quotes vs double quotes:
+#   '' are evaluated dynamically.
+#   "" are evaluated at time of creation and, thereafter, never changes.
+#   alias QS="echo \$PWD"
