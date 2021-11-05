9b8262296b28f068c272a884e9b3cb29ae4bc328
Tin Thai <thaibachthanhtin@gmail.com>
2021-11-05 13:35:26 +0700

    - Reindent - Create more dirs

--- projects/scripts/dotfiles/fresh-install.sh
+++ projects/scripts/dotfiles/fresh-install.sh
@@ -1,46 +1,51 @@
 #!/bin/sh
 
-# Download this script from github repo and execute it shell script.
+# Download this script from github repo and execute it with sh (or where it point to).
 # curl -Ls https://git.io/fresh-install | /bin/sh
 
+CONFIG_BACKUP_DIR="${HOME}/.config~"
+
 function indent {
-   sed 's/^/  -> /'
+  sed 's/^/  -> /'
 }
 
 function config {
-    /usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
+  /usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
 }
 
 #
-# Backup and install.
+# Backup first.
 #
 # Clone my dotfiles repo as a bare repo.
 git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
-CONFIG_BACKUP_DIR="${HOME}/.config~"
 echo 'Attempt to install dotfiles to home folder.'
 config checkout 2>&1 | indent
 # Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
 if [ ${PIPESTATUS[0]} = 0 ]; then
-    echo 'Checked out config.'
+  echo 'Checked out config.'
 else
-    echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
-    mkdir -pv "${CONFIG_BACKUP_DIR}" | indent
-    config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | xargs -I{} mv -v {} "${CONFIG_BACKUP_DIR}" | indent
+  echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
+  mkdir -pv "${CONFIG_BACKUP_DIR}" | indent
+  config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | xargs -I{} mv -v {} "${CONFIG_BACKUP_DIR}" | indent
 fi
 #
-# Checkout and update some configs.
+# Checkout and config.
 #
 config checkout | indent
 config config -- status.showUntrackedFiles no
 # Use vimdiff as default difftool.
 config config -- diff.tool vimdiff
 config config -- difftool.prompt false
+#
+# Bonus.
+#
+# TODO: How to get the directory list from XDG?
 echo 'Creating regular dirs.'
-mkdir -pv ~/{documents,downloads,pictures/screenshots,videos} | indent
+mkdir -pv ~/{documents,downloads,pictures/screenshots,projects/{c,vim},videos} | indent
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
-    echo 'Deleting XDG default dirs.'
-    rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
-    xdg-user-dirs-update
+  echo 'Deleting XDG default dirs.'
+  rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
+  xdg-user-dirs-update
 fi
 echo 'Done.'
