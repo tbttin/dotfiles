e9ae47f826f319b734398b68e8f83719a689e05d
Tin Thai <thaibachthanhtin@gmail.com>
2021-11-15 12:27:13 +0700

    Replace command with path-to-command

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -4,52 +4,52 @@ alias ...='cd ../..'
 # With most commands, you can pass -- as an argument,
 # and all subsequent arguments are treated as operands and not options even if they begin with a dash.
 alias -- --='cd ${OLDPWD}'
-alias d="cd $(xdg-user-dir DOCUMENTS)/"
-alias l="cd $(xdg-user-dir DOWNLOAD)/"
-alias p="cd $(xdg-user-dir PICTURES)/"
-xdg_prj_dir=$(xdg-user-dir PROJECTS)
+alias d="cd $(/usr/bin/xdg-user-dir DOCUMENTS)/"
+alias l="cd $(/usr/bin/xdg-user-dir DOWNLOAD)/"
+alias p="cd $(/usr/bin/xdg-user-dir PICTURES)/"
+xdg_prj_dir=$(/usr/bin/xdg-user-dir PROJECTS)
 alias j="cd ${xdg_prj_dir}/"
-alias s="cd $(xdg-user-dir SCRIPTS)/"
-alias v="cd $(xdg-user-dir VIDEOS)/"
+alias s="cd $(/usr/bin/xdg-user-dir SCRIPTS)/"
+alias v="cd $(/usr/bin/xdg-user-dir VIDEOS)/"
 alias jc="cd ${xdg_prj_dir}/c/"
 alias jv="cd ${xdg_prj_dir}/vim/"
 
 # Quick open some config files.
 # Bashes.
-alias vb='vim ~/.bashrc'
-alias vp='vim ~/.bash_profile'
-alias va='vim ${XDG_CONFIG_HOME}/bash/aliases.bash'
+alias vb='/usr/bin/vim ~/.bashrc'
+alias vp='/usr/bin/vim ~/.bash_profile'
+alias va='/usr/bin/vim ${XDG_CONFIG_HOME}/bash/aliases.bash'
 # Readline.
-alias vn='vim ${INPUTRC}'
+alias vn='/usr/bin/vim ${INPUTRC}'
 # Dynamic window manager.
-alias vd='vim ${XDG_CONFIG_HOME}/dwm-git/config.h'
+alias vd='/usr/bin/vim ${XDG_CONFIG_HOME}/dwm-git/config.h'
 # Suckless terminal.
-alias vs='vim ${XDG_CONFIG_HOME}/st-git/config.h'
+alias vs='/usr/bin/vim ${XDG_CONFIG_HOME}/st-git/config.h'
 # Vims.
-alias vv='vim ${XDG_CONFIG_HOME}/vim/vimrc'
-alias vc='vim ${XDG_CONFIG_HOME}/vim/ftplugin/c.vim'
+alias vv='/usr/bin/vim ${XDG_CONFIG_HOME}/vim/vimrc'
+alias vc='/usr/bin/vim ${XDG_CONFIG_HOME}/vim/ftplugin/c.vim'
 # xinitrc.
-alias vx='vim ${XINITRC}'
+alias vx='/usr/bin/vim ${XINITRC}'
 # Zathura.
-alias vz='vim ${XDG_CONFIG_HOME}/zathura/zathurarc'
+alias vz='/usr/bin/vim ${XDG_CONFIG_HOME}/zathura/zathurarc'
 # Makefie template.
-alias vm='vim ${XDG_CONFIG_HOME}/vim/tmplts/Makefile'
+alias vm='/usr/bin/vim ${XDG_CONFIG_HOME}/vim/tmplts/Makefile'
 
 # Adding colors.
-alias ls='ls --color=auto'
+alias ls='/usr/bin/ls --color=auto'
 alias ll='ls -lh'
 alias la='ll -a'
-alias grep='grep --color=auto'
-alias ip='ip -color=auto'
-alias diff='diff -color=auto'
+alias grep='/usr/bin/grep --color=auto'
+alias ip='/usr/bin/ip -color=auto'
+alias diff='/usr/bin/diff -color=auto'
 
 # [U]mount external drives/USBs.
-alias m16='doas mount /mnt/16g-usb && cd /mnt/16g-usb'
-alias um16='doas umount /mnt/16g-usb'
+alias m16='/usr/bin/doas /usr/bin/mount /mnt/16g-usb && cd /mnt/16g-usb'
+alias um16='/usr/bin/doas /usr/bin/umount /mnt/16g-usb'
 
 # System stuffs.
-alias dapm='doas pacman'
-alias spo='systemctl poweroff -i'
+alias dapm='/usr/bin/doas /usr/bin/pacman'
+alias spo='/usr/bin/systemctl /usr/bin/poweroff -i'
 
 # Bare git dotfiles aliases.
 alias config="/usr/bin/git --work-tree=${HOME} --git-dir=${HOME}/.config/dotfiles/"
@@ -63,12 +63,12 @@ alias cfp='config push'
 
 # For the sake of videos and movies.
 # Decrease seek_interval by 5, suppress printing copyright, build options and versions.
-alias ffplay='ffplay -hide_banner -nostats -seek_interval 5 -autoexit'
+alias ffplay='/usr/bin/ffplay -hide_banner -nostats -seek_interval 5 -autoexit'
 # Seeking for file infomation.
-alias ffprobe='ffprobe -hide_banner'
+alias ffprobe='/usr/bin/ffprobe -hide_banner'
 
 # Start X with xinitrc in $XDG_CONFIG_HOME.
-alias startx='startx ${XINITRC}'
+alias startx='/usr/bin/startx ${XINITRC}'
 
 # Display all locations containing an executable named includes aliases, builtins, and functions.
 alias type='type -a'
