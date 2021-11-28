15247acf05b4cf617bcfd795f89159e2ebb6f8a1
Tin Thai <thaibachthanhtin@gmail.com>
2021-11-28 18:06:24 +0700

    Use long option form, remove /usr/bin/

--- projects/scripts/dotfiles/fresh-install.sh
+++ projects/scripts/dotfiles/fresh-install.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
 # Download this raw script from github and execute it with bash:
-#   curl -Ls https://git.io/fi.sh | /bin/bash
+#   curl --silent --location https://git.io/fi.sh | /bin/bash
 # Shorten URL with git.io and curl:
-#   curl -i https://git.io -F "url=YOUR_GITHUB_URL_HERE" -F "code=YOUR_CUSTOM_NAME"
+#   curl --inclulde https://git.io --form "url=YOUR_GITHUB_URL_HERE" --form "code=YOUR_CUSTOM_NAME"
 
 CONFIG_BACKUP_DIR="${HOME}/.config~"
 
 indent()
 {
-  /usr/bin/sed 's/^/ > /'
+  sed 's/^/ > /'
 }
 
 config()
 {
-  /usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
+  git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
 }
 
 #
 # Backup first.
 #
 # Clone my dotfiles repo as a bare repo.
-/usr/bin/git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
+git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
-/usr/bin/echo 'Attempt to install dotfiles to home folder.'
+echo 'Attempt to install dotfiles to home folder.'
 config checkout 2>&1 | indent
 # Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
 if [ ${PIPESTATUS[0]} = 0 ]; then
-  /usr/bin/echo 'Checked out config.'
+  echo 'Checked out config.'
 else
-  /usr/bin/echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
-  /usr/bin/mkdir -pv "${CONFIG_BACKUP_DIR}" | indent
-  config checkout 2>&1 | /usr/bin/egrep '\s+\.' | /usr/bin/awk {'print $1'} | \
-    /usr/bin/xargs -I{} mv -v {} "${CONFIG_BACKUP_DIR}" | indent
+  echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
+  mkdir --parrents --verbose "${CONFIG_BACKUP_DIR}" | indent
+  config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} | \
+    xargs -I{} mv --verbose {} "${CONFIG_BACKUP_DIR}" | indent
 fi
 #
 # Checkout and config.
@@ -48,12 +48,12 @@ config submodule update --init --recursive
 # Bonus.
 #
 # TODO: How to get the directory list from XDG?
-/usr/bin/echo 'Creating regular dirs.'
-/usr/bin/mkdir -pv ~/{documents,downloads,pictures/ss,projects/{c,vim},videos} | indent
+echo 'Creating regular dirs.'
+mkdir --parrents --verbose ~/{documents,downloads,pictures/ss,projects/{c,vim},videos} | indent
 if [ -x "$(command -v xdg-user-dirs-update)" ]; then
-  /usr/bin/echo 'Deleting XDG default dirs.'
-  /usr/bin/rmdir -v --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
-  /usr/bin/xdg-user-dirs-update
+  echo 'Deleting XDG default dirs.'
+  rmdir --verbose --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
+  xdg-user-dirs-update
 fi
-/usr/bin/echo 'Done.'
+echo 'Done.'
 
