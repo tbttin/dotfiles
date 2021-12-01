00e90167f86b8d8d9b2f966b8bde617b3c799af7
Tin Thai <thaibachthanhtin@gmail.com>
2021-12-01 23:38:17 +0700

    Enable ffplay switchtotag

--- .config/dwm-git/config.h
+++ .config/dwm-git/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
     /*  class,     instance, title, tags mask, switchtotag, isfloating, monitor */
       { "firefox", NULL,     NULL,  1 << 8,    1,           0,          -1 },
       { "Zathura", NULL,     NULL,  1 << 7,    1,           0,          -1 },
-      { "ffplay",  NULL,     NULL,  1 << 2,    0,           0,          -1 },
+      { "ffplay",  NULL,     NULL,  1 << 2,    1,           0,          -1 },
 };
 
 /* layout(s) */
