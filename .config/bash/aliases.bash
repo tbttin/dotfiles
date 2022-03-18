ea05b468f81c30e6d4a471c7d82030ebfa91137e
Tin Thai <thaibachthanhtin@gmail.com>
2022-03-18 09:51:36 +0700

    Re-arrange, reformat

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -1,8 +1,9 @@
 # Faster navigation.
 alias ..='cd ..'
 alias ...='cd ../..'
-# With most commands, you can pass -- as an argument,
-# and all subsequent arguments are treated as operands and not options even if they begin with a dash.
+# With most commands, you can pass -- as an argument, and all subsequent
+# arguments are treated as operands and not options even if they begin with a
+# dash ('-').
 alias -- --='cd ${OLDPWD}'
 alias d="cd $(/usr/bin/xdg-user-dir DOCUMENTS)/"
 alias l="cd $(/usr/bin/xdg-user-dir DOWNLOAD)/"
@@ -39,40 +40,23 @@ alias r='/usr/bin/make run --'
 # Take note.
 alias vn="/usr/bin/vim ${xdg_prj_dir}/notes/index.md"
 
-# Unset local variables.
-unset xdg_prj_dir
-
 # Adding colors.
 alias ls='/usr/bin/ls --color=auto'
-alias la='ls --all'
-alias ll='ls -l --all --human-readable'
 alias grep='/usr/bin/grep --color=auto'
 alias ip='/usr/bin/ip -color=auto'
 alias diff='/usr/bin/diff --color=auto'
 
-# [U]mount external drives/USBs.
-# alias m16='/usr/bin/doas /usr/bin/mount /mnt/16g-usb && cd /mnt/16g-usb'
-# alias um16='/usr/bin/doas /usr/bin/umount /mnt/16g-usb'
+# List files.
+alias la='ls --all'
+alias ll='ls -l --all --human-readable'
 
-# System stuff.
-alias dpm='/usr/bin/doas /usr/bin/pacman'
-alias spo='/usr/bin/systemctl poweroff -i'
+# Display all locations containing an executable named includes aliases,
+# builtins, and functions.
+alias type='type -a'
 
-# Bare git dotfiles aliases.
-alias config='/usr/bin/git --work-tree=${HOME} --git-dir=${HOME}/.config/dotfiles/'
-alias cfa='config add'
-alias cfap='config add --patch'
-alias cfc='config commit'
-alias cfd='config diff'
-alias cfdi='config diff --staged'
-alias cfdt='config difftool'
-alias cfl='config log'
-alias cfp='config push'
-alias cfr='config restore'
-alias cfri='config restore --staged'
-alias cfs='config status'
+# Shorthand for transmission-remote command.
+alias tmr='/usr/bin/transmission-remote'
 
-# For the sake of videos and movies.
 # Do not print copyright, build options and versions; set seek_interval to 3,
 # start in full-screen mode.
 alias ffplay='/usr/bin/ffplay -hide_banner -nostats -seek_interval 3 -autoexit -fs'
@@ -82,11 +66,26 @@ alias ffprobe='/usr/bin/ffprobe -hide_banner'
 # Start X with "~/.config/xorg/xinitrc"
 alias startx="/usr/bin/startx '${XINITRC}'"
 
-# Display all locations containing an executable named includes aliases, builtins, and functions.
-alias type='type -a'
+# System stuff.
+alias dpm='/usr/bin/doas /usr/bin/pacman'
+alias spo='/usr/bin/systemctl poweroff -i'
 
-# Shorthand for transmission-remote command.
-alias tmr='/usr/bin/transmission-remote'
+# Bare git dotfiles aliases.
+alias config='/usr/bin/git --work-tree=${HOME} --git-dir=${HOME}/.config/dotfiles/'
+alias cfa='config add'
+alias cfap='config add --patch'
+alias cfc='config commit --verbose'
+alias cfd='config diff'
+alias cfdi='config diff --staged'
+alias cfdt='config difftool'
+alias cfl='config log --name-only'
+alias cfp='config push'
+alias cfr='config restore'
+alias cfri='config restore --staged'
+alias cfs='config status'
+
+# Unset local variables.
+unset xdg_prj_dir
 
 # Notes {{{1
 # Aliases are created and listed with the alias command, and removed with the unalias command.
