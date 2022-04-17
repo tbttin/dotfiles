ef3d732e7b20635bd682cbfee8cc3fc6ae604438
Tin Thai <thaibachthanhtin@gmail.com>
2022-04-17 15:30:15 +0700

    Use path-to-binary-files

--- projects/scripts/dotfiles/fresh-install.sh
+++ projects/scripts/dotfiles/fresh-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Download this raw script from github and execute it with bash:
+# Installation: download this raw script from github and execute it with Bash.
 #   curl --silent --location https://git.io/fi.sh | /bin/bash
 # Shorten URL with git.io and curl:
 #   curl --inclulde https://git.io --form "url=YOUR_GITHUB_URL_HERE" --form "code=YOUR_CUSTOM_NAME"
@@ -9,31 +9,31 @@ CONFIG_BACKUP_DIR="${HOME}/.config~"
 
 indent()
 {
-	sed 's/^/ > /'
+	/usr/bin/sed 's/^/ > /'
 }
 
 config()
 {
-	git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
+	/usr/bin/git --git-dir="${HOME}/.config/dotfiles" --work-tree="${HOME}" "$@"
 }
 
 #
 # Backup first
 #
 # Clone my dotfiles repo as a bare repo.
-git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
+/usr/bin/git clone --bare 'https://github.com/tbttin/dotfiles.git' "${HOME}/.config/dotfiles"
 # Backup stock config files if they exist and install my dotfiles.
-echo 'Attempt to install dotfiles to home directory.'
+/usr/bin/echo 'Attempt to install dotfiles to home directory.'
 config checkout 2>&1 | indent
 # Piped command exit status: ${PIPESTATUS[0]}, in zsh: ${pipestatus[1]}.
 if test ${PIPESTATUS[0]} = 0
 then
-	echo 'Checked out configs.'
+	/usr/bin/echo 'Checked out configs.'
 else
-	echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
-	mkdir --parrents --verbose "${CONFIG_BACKUP_DIR}" | indent
-	config checkout 2>&1 | egrep '\s+\.' | awk {'print $1'} |
-		xargs -I{} mv --verbose {} "${CONFIG_BACKUP_DIR}" | indent
+	/usr/bin/echo "Backing up pre-existing dotfiles to '${CONFIG_BACKUP_DIR}'."
+	/usr/bin/mkdir --parrents --verbose "${CONFIG_BACKUP_DIR}" | indent
+	config checkout 2>&1 | /usr/bin/egrep '\s+\.' | /usr/bin/awk {'print $1'} |
+		xargs -I{} /usr/bin/mv --verbose {} "${CONFIG_BACKUP_DIR}" | indent
 	config checkout | indent
 fi
 # Some configs.
@@ -47,11 +47,11 @@ config submodule update --init --recursive
 #
 # Preparing $HOME
 #
-mkdir --parrents --verbose ~/{documents,downloads,pictures/ss,projects/{c,vim},videos} | indent
-if type -t xdg-user-dirs-update >/dev/null
+/usr/bin/mkdir --parrents --verbose ~/{documents,downloads,pictures/ss,projects/{c,vim},videos} | indent
+if type -t /usr/bin/xdg-user-dirs-update >/dev/null
 then
-	rmdir --verbose --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
-	xdg-user-dirs-update
+	/usr/bin/rmdir --verbose --ignore-fail-on-non-empty ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos} | indent
+	/usr/bin/xdg-user-dirs-update
 fi
-echo 'Done.'
+/usr/bin/echo 'Done.'
 
