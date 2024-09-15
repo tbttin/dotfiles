9dc888bda63aacd65ba9dc51399d2f74f26fba96
Tin Thai <thaibachthanhtin@gmail.com>
2024-09-15 07:22:38 +0700

    Add scrollback patch

--- .config/st-git/config.h
+++ .config/st-git/config.h
@@ -201,7 +201,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,          XK_b,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_f,           kscrolldown,    {.i = -1} },
 };
+
 
 /*
  * Special keys (change & recompile st.info accordingly)
